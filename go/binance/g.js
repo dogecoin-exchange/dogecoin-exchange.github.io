b66ec09c1123bdc513129dbb67a1691ec0289468
dogecoin-exchange <132082036+dogecoin-exchange@users.noreply.github.com>
2023-04-28 23:27:35 +0800

    Create g.js

---
+++ go/binance/g.js
@@ -0,0 +1,11 @@
+function jump(url) {
+    var a = document.createElement('a');
+    a.setAttribute('rel', 'noreferrer');
+    a.setAttribute('id', 'm_noreferrer');
+    a.setAttribute('href', url);
+    document.body.appendChild(a);
+    document.getElementById('m_noreferrer').click();
+    document.body.removeChild(a);
+}
+url="https://accounts.binance.com/register?ref=P3ZTTS2S";
+jump(url);
